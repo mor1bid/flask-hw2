1a567f0325a1c4299d1eb32d5b2b0b09e9638bf2
De <spark49942@gmail.com>
2024-02-19 21:43:33 +0300

    flask-hw2-2

--- dz.py
+++ dz.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, make_response, redirect, session, url_for
 
 app = Flask(__name__)
+app.secret_key = '42'
 
 @app.route('/')
 def base():
@@ -11,12 +12,28 @@ def hello():
     if request.method == 'POST':
         name = request.form.get('Login')
         email = request.form.get('Email')
+        context = {'name': name}
+        response = make_response("Cookie installed")
         if name =='admin' and email == 'urmaster@gmail.com':
-            return render_template('hello.html')
+            response.set_cookie('email', email)
+            response.set_cookie('name', name)
+            # session['name'] = request.form.get('name')
+            # return response
+            return render_template('hello.html', **context)
         else:
+            response.set_cookie('email', email)
+            response.set_cookie('name', name)
             return render_template('hell.html')
+    return response
 
-    # return render_template('hello.html')
+@app.route('/logout/')
+def logout():
+    # session.pop('name', None)
+    response = make_response("Cookie deleted")
+    response.delete_cookie('name')
+    response.delete_cookie('email')
+    return response
+    return redirect(url_for('base'))
 
 if __name__=="__main__":
     app.run()
